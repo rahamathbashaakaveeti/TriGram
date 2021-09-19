23219d03b648271e41f46767cca6149ac808ff2d
rahamathbashaakaveeti <rahamath.basha@gmail.com>
2021-09-19 17:10:08 +0100

    Initial Commit

---
+++ trigramdataproject/src/main/java/com/jpmc/trigramdataproject/App.java
@@ -0,0 +1,13 @@
+package com.jpmc.trigramdataproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
